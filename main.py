1f9d0998222e5cf02eec7f6ffe594b8e72fd76e9
John Sequera <jw.sequera@gmail.com>
2024-05-03 01:13:02 -0400

    cambios en la interfaz y montecarlo.py anadido

--- main.py
+++ main.py
@@ -1,61 +1,71 @@
 import flet as ft
 
+#DIMENSIONES 1300X900 PX
+
 def main(page: ft.Page):
     page.title = "Flet counter example"
+    # page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    # page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
     def servidoresChange(e):
-        if e.control.value.contains("a"):
-                e.control.value = e.control.value.replace("a", "")
-                page.update()
+        print(e.control.value)
 
-        else:
-            serviciosText.value = e.control.value
-            page.update()
-
+    def iniciarSimulacion(e):
+        print("Simulacion iniciada")
+        print("Numero de servidores: ", servidoresText.value)
+        print("Tasa de clientes: ", clientesText.value)
+        print("Tasa de servicios: ", serviciosText.value)
+        print("Tiempo de ejecucion: ", tiempoEjecucionText.value)
 
     servidoresText = ft.TextField(
         label = "Numero de servidores",
-        value = 0,
-        on_change= servidoresChange
+        input_filter= ft.InputFilter(allow=True, regex_string=r"[0-9]", replacement_string=""),
             
     )
 
     clientesText = ft.TextField(
         label = "Tasa de clientes",
-        value = 0
+        input_filter= ft.InputFilter(allow=True, regex_string=r"[0-9]", replacement_string=""),
     )
 
     serviciosText = ft.TextField(
         label = "Tasa de servicios",
-        value = 0
+        input_filter= ft.InputFilter(allow=True, regex_string=r"[0-9]", replacement_string=""),
+    )
+
+    tiempoEjecucionText = ft.TextField(
+        label = "Tiempo de ejecucion (horas)",
+        input_filter= ft.InputFilter(allow=True, regex_string=r"[0-9]", replacement_string=""),
     )
 
     btnEjecutar = ft.ElevatedButton(
         text="Ejecutar",
-        width= 300
+        width= 500,
+        height=60,
+        on_click= iniciarSimulacion
         )
     btnReiniciar = ft.ElevatedButton(
         text="Reiniciar",
-        width= 300
+        width= 500,
+        height=60
         )
 
-    inputCol = ft.Column(
-            [
-                servidoresText,
-                clientesText,
-                serviciosText,
-                btnEjecutar,
-                btnReiniciar
-            ],
-            height= 600,
-            spacing=50
-        )
+    container = ft.Column(
+        [
+            servidoresText,
+            clientesText,
+            serviciosText,
+            tiempoEjecucionText,
+            btnEjecutar,
+            btnReiniciar
+        ],
+        width=500,
+        height=100,
+        expand=True,
+        alignment=ft.MainAxisAlignment.SPACE_EVENLY
+    )
 
-    mainRow = ft.Row([
-        inputCol,
-    ])
-
-    page.add(mainRow)
+    page.add(container)
 
 
 
